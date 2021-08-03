e6ca416a3ccc4f31923869eef690391f659fc266
Kiichi Tachibana <bodega1123@gmail.com>
2021-08-03 18:30:28 +0900

    Fix client

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!,
-  apiKey: process.env.NEXT_PUBLIC_API_KEY!
+  serviceDomain: 'nextjs-micro-cms-blog',
+  apiKey: process.env.API_KEY || ''
 })
